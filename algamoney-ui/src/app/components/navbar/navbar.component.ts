1ca4df06e2244475e152bef40e53e134a69c9907
jonataslaet <jonataslaetprogramador@gmail.com>
2022-04-29 14:44:36 -0300

    projeto003: Adição de menu intercambiável
    Implementação de toogle para mostrar ou não o menu

--- algamoney-ui/src/app/components/navbar/navbar.component.ts
+++ algamoney-ui/src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  showNavbarMenu = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleShowNavbarMenu() {
+    this.showNavbarMenu = !this.showNavbarMenu;
   }
 
 }
