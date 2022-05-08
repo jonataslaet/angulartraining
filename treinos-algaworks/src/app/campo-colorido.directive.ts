4c2d6962435cd3cfa3ee50bfc83b66548280b267
jonataslaet <jonataslaetprogramador@gmail.com>
2022-05-07 23:17:10 -0300

    projeto002: Vinculando propriedades do hospedeiro com @HostBinding

--- treinos-algaworks/src/app/campo-colorido.directive.ts
+++ treinos-algaworks/src/app/campo-colorido.directive.ts
@@ -1,25 +1,20 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCampoColorido]'
 })
 export class CampoColoridoDirective {
 
-  constructor(
-    private elementRef: ElementRef,
-    private render: Renderer2
-  ) {
-    
-  }
+  @HostBinding('style.backgroundColor') corDeFundo: string;
 
   @HostListener('focus')
   onFocus() {
-    this.render.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+    this.corDeFundo = 'yellow';
   }
 
   @HostListener('blur')
   offFocus() {
-    this.render.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.corDeFundo = 'transparent';
   }
 
 }
